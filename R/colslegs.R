f7ea73201298d3dd841db60ff332d95f67045b3d
Roger Bivand <Roger.Bivand@nhh.no>
2003-12-12 00:00:00 +0000

    version 0.3-4

--- R/colslegs.R
+++ R/colslegs.R
@@ -1,32 +1,40 @@
 # Copyright 2000-2001 (c) Nicholas Lewin-Koh 
-# modifications 2001-2003 Roger Bivand
+# modifications 2001-2003 (c) Roger Bivand
 
 
 # Returns a vector of HSV values
-color.ramp <- function(nclass, color='red', nvec=NULL, type='q') {
-  eq.split <- function(ncl) {
-    mult <- rep((1/ncl), (ncl-1))
-    mult*seq(1, (ncl-1))
-  }
-  color.list <- list(cname=c("blue","green","yellow","red"),
-                   hsvcol=c(.7,.375,.19,0))
+# modifications 2003 (c) Renaud Lancelot
+color.ramp <- function (nclass, color = "red", nvec = NULL, type = "q"){
+  eq.split <- function(ncl){
+    mult <- rep((1 / ncl), (ncl - 1))
+    mult * seq(1, (ncl - 1))
+    }
+  color.list <- list(cname = c("blue", "green", "yellow", "red"), hsvcol = c(0.7, 0.375, 0.19, 0))
   cind <- match(color, color.list$cname)
-  if(nvec) {
-     if(type=='q') {
-       pr <- eq.split(nclass)
-       brks <- c(min(nvec), quantile(nvec, pr, names=FALSE), max(nvec))
-       classvec <- cut(nvec, brks, labels=FALSE, include.lowest=T)
-       ramp <- hsv(rep(color.list$hsvcol[cind], nclass), c(pr, 1))  
-       return(list(ramp=ramp, col.class=classvec))
-     }
-     else if(type=='e') {
-       pr <- eq.split(nclass)
-       brks <- c(min(nvec), pr*diff(range(nvec)), max(nvec))
-       classvec <- cut(nvec, brks, labels=FALSE, include.lowest=T)
-       ramp <- hsv(rep(color.list$hsvcol[cind], nclass), c(pr, 1))  
-       return(list(ramp=ramp, col.class=classvec)) 
-     }
-  }
+### change from "if(nvec)" to "if(!is.null(nvec))"
+  if(!is.null(nvec)){
+    if(type == "q"){
+      pr <- eq.split(nclass)
+### changes in min, quantile and max
+      brks <- c(min(nvec, na.rm = TRUE),
+                quantile(nvec, pr, names = FALSE, na.rm = TRUE),
+                max(nvec, na.rm = TRUE))
+      classvec <- cut(nvec, brks, labels = FALSE, include.lowest = TRUE)
+      ramp <- hsv(rep(color.list$hsvcol[cind], nclass), c(pr, 1))
+      return(list(ramp = ramp, col.class = classvec))
+      }
+  else
+    if(type == "e"){
+      pr <- eq.split(nclass)
+### changes in min, range and max
+      brks <- c(min(nvec, na.rm = TRUE),
+                pr * diff(range(nvec, na.rm = TRUE)),
+                max(nvec, na.rm = TRUE))
+      classvec <- cut(nvec, brks, labels = FALSE, include.lowest = TRUE)
+      ramp <- hsv(rep(color.list$hsvcol[cind], nclass), c(pr, 1))
+      return(list(ramp = ramp, col.class = classvec))
+      }
+    }
   return(NULL)
 }
 
@@ -39,9 +47,9 @@ leglabs <- function(vec, under="under", over="over", between="-") {
 	res
 }
 
-findInterval2 <- function (x, vec, rightmost.closed = FALSE, all.inside = TRUE) 
+findInterval2 <- function (y, vec, rightmost.closed = FALSE, all.inside = TRUE) 
 {
-    nx <- length(x)
+    nx <- length(y)
     if (any(is.na(vec) | is.nan(vec))) stop ("NAs found in vec")
     if (is.unsorted(vec)) 
         stop("`vec' must be sorted non-decreasingly")
@@ -49,7 +57,7 @@ findInterval2 <- function (x, vec, rightmost.closed = FALSE, all.inside = TRUE)
     if (vec[length(vec)] == Inf) 
 	vec[length(vec)] <- .Machine$double.xmax
     .C("find_interv_vec", xt = as.double(vec), n = length(vec), 
-        x = as.double(x), nx = nx, as.logical(rightmost.closed), 
+        x = as.double(y), nx = nx, as.logical(rightmost.closed), 
         as.logical(all.inside), index = integer(nx), DUP = FALSE,
 	PACKAGE = "base")$index
 }
---
+++ R/subset.R
@@ -0,0 +1,21 @@
+# Copyright 2003 (c) Roger Bivand
+
+subset.polylist <- function(x, subset, fit.bbox=TRUE, ...) {
+	if (!inherits(x, "polylist")) stop("x not a polylist object")
+	if (!is.logical(subset)) stop("subset not a logical vector")
+	if (length(x) != length(subset)) stop("x and subset different lengths")
+	res <- subset.default(x, subset)
+	attr(res, "region.id") <- subset.default(attr(x, "region.id"), subset)
+	class(res) <- "polylist"
+	attr(res, "maplim") <- attr(x, "maplim")
+	if (fit.bbox) attr(res, "maplim") <- maplimFromBbox(res)
+	res
+}
+
+maplimFromBbox <- function(plist) {
+	bboxes <- sapply(plist, function(x) attr(x, "bbox"))
+	mapxlim <- range(c(bboxes[c(1,3),]))
+	mapylim <- range(c(bboxes[c(2,4),]))
+	list(x=mapxlim, y=mapylim)
+}
+
