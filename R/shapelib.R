d27888e6d0c9bb4a47f2c68e0c79005022e09900
Roger Bivand <Roger.Bivand@nhh.no>
2004-12-29 00:00:00 +0000

    version 0.4-9

--- R/shapelib.R
+++ R/shapelib.R
@@ -49,79 +49,7 @@ print.shapehead <- function(x, ...) {
       x[[3]], "\n")
 }
 
-#dbf.read <- function(filen) {
-#  filen <- path.expand(filen)
-#  df <- .Call("Rdbfread", as.character(filen), PACKAGE="maptools")
-#  onames <- names(df)
-#  inames <- make.names(onames, unique=TRUE)
-#  names(df) <- inames
-#  if (!(identical(onames, inames))) {
-#    for (i in 1:length(onames))
-#      if (!(identical(onames[i], inames[i]))) 
-#        cat("Field name: ", onames[i], " changed to: ", inames[i], "\n")
-#  }
-#  df <- data.frame(lapply(df,
-#    function(x) {if(is.character(x)) {factor(x)} else x }))
-#  df
-##}
 
-
-#dbf.write <- function(dataframe, filename, factor2char=TRUE) {
-## need to check precision, and that factors are converted to character
-## how to handle NAs?
-#  if (!is.data.frame(dataframe)) stop("not a data frame")
-#  if (!all(complete.cases(dataframe))) stop("NAs not permitted")
-#  if (any(sapply(dataframe, function(x) !is.null(dim(x)))))
-#    stop("Can't handle multicolumn columns")
-#  if (factor2char) {
-#    dataframe <- as.data.frame(lapply(dataframe, function(x) {
-#      if (is.factor(x)) {
-#        x <- as.character(x)
-#        class(x) <- "AsIs"
-#        x
-#     } else x } ))
-#  } else {
-#    dataframe <- as.data.frame(lapply(dataframe, function(x) {
-#      if (is.factor(x)) {
-#        x <- as.integer(x)
-#        x
-#     } else x } ))
-#  }
-#  dataframe <- as.data.frame(lapply(dataframe, function(x) {
-#    if (is.logical(x)) {
-#      x <- as.integer(x)
-#      x
-#    } else x } ))
-#  m <- ncol(dataframe)
-#  precision <- integer(m)
-#  scale <- integer(m)
-#  dfnames <- names(dataframe)
-#  for (i in 1:m) {
-#    nlen <- nchar(dfnames[i])
-#    if (is.integer(dataframe[,i])) {
-#      rx <- range(dataframe[,i])
-#      mrx <- as.integer(max(ceiling(log10(abs(rx))))+3)
-#      precision[i] <- as.integer(max(nlen, mrx))
-#      if (precision[i] > 19) precision[i] <- as.integer(19)
-#      scale[i] <- as.integer(0)
-#    } else if (is.double(dataframe[,i])) {
-#      precision[i] <- as.integer(19)
-#      rx <- range(dataframe[,i])
-#      mrx <- as.integer(max(ceiling(log10(abs(rx)))))
-#      scale[i] <- as.integer(precision[i] - ifelse(mrx > 0, mrx+3, 3))
-#      if (scale[i] > 15) scale[i] <- as.integer(15)
-#    } else if (is.character(dataframe[,i])) {
-#      mf <- max(nchar(dataframe[,i]))
-#      precision[i] <- as.integer(max(nlen, mf))
-#      if (precision[i] > 254) precision[i] <- as.integer(254)
-#      scale[i] <- as.integer(0)
-#    } else stop("unknown column type in data frame")
-#  }
-#  invisible( .External("DoWritedbf", as.character(filename), 
-#    dataframe, as.integer(precision), as.integer(scale), PACKAGE="maptools"))
-#}
-#
-#
 write.pointShape <- function(object, file, coordinates, factor2char=TRUE) {
   file <- path.expand(file)
   if (nchar(basename(file)) > 8) 
@@ -145,6 +73,29 @@ write.polylistShape <- function(polylist, df, file, factor2char=TRUE) {
   if (!inherits(polylist, "polylist")) stop("not a polylist object")
   if (nrow(df) != length(polylist))
     stop("different number of rows in polylist and data frame")
+  if (!any(sapply(polylist, function(x) is.double(x)))) {
+    for (i in 1:length(polylist)) { 
+      a <- attributes(polylist[[i]])
+      polylist[[i]] <- matrix(as.double(polylist[[i]]), ncol=2)
+      attributes(polylist[[i]]) <- a
+    }
+    warning("coordinates changed to double")
+  }
+  if (!any(sapply(polylist, function(x) is.integer(attr(x, "nParts"))))) {
+    for (i in 1:length(polylist)) attr(polylist[[i]], "nParts") <- 
+		as.integer(attr(polylist[[i]], "nParts"))
+    warning("nParts changed to integer")
+  }
+  if (!any(sapply(polylist, function(x) is.integer(attr(x, "pstart")$from)))) {
+    for (i in 1:length(polylist)) attr(polylist[[i]], "pstart")$from <- 
+		as.integer(attr(polylist[[i]], "pstart")$from)
+    warning("pstart$from changed to integer")
+  }
+  if (!any(sapply(polylist, function(x) is.integer(attr(x, "pstart")$to)))) {
+    for (i in 1:length(polylist)) attr(polylist[[i]], "pstart")$to <- 
+		as.integer(attr(polylist[[i]], "pstart")$to)
+    warning("pstart$to changed to integer")
+  }
   library(foreign)
   write.dbf(df, paste(file, ".dbf", sep=""), factor2char=factor2char)
   res <- .Call("shpwritepolys", as.character(file), polylist, 
