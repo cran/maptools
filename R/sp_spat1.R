e6943f78652403235f62c9afc2c409ef95defb68
Roger Bivand <Roger.Bivand@nhh.no>
2007-01-16 15:43:02 +0000

    version 0.6-6

--- R/sp_spat1.R
+++ R/sp_spat1.R
@@ -5,13 +5,16 @@ if (!isClass("ppp"))
 if (!isClass("owin"))
 	setClass("owin")
 
+if (!isClass("im"))
+    setClass("im")
+
 as.SpatialPoints.ppp =  function(from) SpatialPoints(cbind(from$x, from$y))
-setAs("ppp", "SpatialPoints", as.SpatialPoints.ppp)
+#setAs("ppp", "SpatialPoints", as.SpatialPoints.ppp)
 
 as.SpatialPointsDataFrame.ppp = function(from) 
 	SpatialPointsDataFrame(SpatialPoints(cbind(from$x, from$y)), 
-		list(marks = from$marks))
-setAs("ppp", "SpatialPointsDataFrame", as.SpatialPointsDataFrame.ppp)
+		data.frame(marks = from$marks))
+#setAs("ppp", "SpatialPointsDataFrame", as.SpatialPointsDataFrame.ppp)
 
 as.SpatialGridDataFrame.ppp = function(from) {
 	require(spatstat)
@@ -27,4 +30,27 @@ as.SpatialGridDataFrame.ppp = function(from) {
 	data = data.frame(mask = as.vector(m))
 	SpatialGridDataFrame(gt, data)
 }
-setAs("ppp", "SpatialGridDataFrame", as.SpatialGridDataFrame.ppp)
+#setAs("ppp", "SpatialGridDataFrame", as.SpatialGridDataFrame.ppp)
+
+as.SpatialGridDataFrame.im = function(from) {
+    require(spatstat)
+    offset = c(from$xrange[1] + 0.5 * from$xstep, from$yrange[1] + 
+        0.5 * from$ystep)
+    cellsize = c(diff(from$xrange)/from$dim[2], diff(from$yrange)/from$dim[1])
+    dim = c(from$dim[2], from$dim[1])
+    gt = GridTopology(offset, cellsize, dim)
+    m = t(from$v[nrow(from$v):1,])
+    data = data.frame(v = as.vector(m))
+    SpatialGridDataFrame(gt, data)
+}
+#setAs("im", "SpatialGridDataFrame", as.SpatialGridDataFrame.im)
+
+
+as.im.SpatialGridDataFrame = function(from) {
+    require(spatstat)
+    xi <- sp:::as.image.SpatialGridDataFrame(from)
+    im(t(xi$z), xcol=xi$x, yrow=xi$y)
+}
+setAs("SpatialGridDataFrame", "im", as.im.SpatialGridDataFrame)
+
+
