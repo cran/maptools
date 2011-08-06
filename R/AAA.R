54a291a5ae4e3c379571e6043c1da1b2acb4e07b
Roger Bivand <Roger.Bivand@nhh.no>
2011-08-06 00:00:00 +0000

    version 0.8-10

--- R/AAA.R
+++ R/AAA.R
@@ -1,19 +1,16 @@
 .MAPTOOLS_CACHE <- new.env(FALSE, parent=globalenv())
 
 .onLoad <- function(lib, pkg) {
-    require(methods, quietly = TRUE, warn.conflicts = FALSE)
-    require("sp")
     assign("gpclib", FALSE, envir=.MAPTOOLS_CACHE)
-    Smess <- paste("\n\tNote: polygon geometry computations in maptools\n",
-              "\tdepend on the package gpclib, which has a\n",
-              "\trestricted licence. It is disabled by default;\n",
-              "\tto enable gpclib, type gpclibPermit()\n")
-    Smess <- paste(Smess,
-        "\nChecking rgeos availability as gpclib substitute:\n")
-    rgeosI <- "rgeos" %in% .packages(all = TRUE)
+    Smess <- paste("Checking rgeos availability: ")
+    rgeosI <- setRgeosStatus()
     Smess <- paste(Smess, rgeosI, "\n", sep="")
+    if (!rgeosI) Smess <- paste(Smess, 
+              "\tNote: when rgeos is not available, polygon geometry",
+              "\tcomputations in maptools depend on gpclib,\n",
+              "\twhich has a restricted licence. It is disabled by default;\n",
+              "\tto enable gpclib, type gpclibPermit()\n")
     packageStartupMessage(Smess, appendLF = FALSE)
-    assign("rgeos", rgeosI, envir=.MAPTOOLS_CACHE)
 }
 
 .onUnload <- function(libpath) {
