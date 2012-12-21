3b479e28fb946ddc3a0f1cd09aa82e987119931c
Roger Bivand <Roger.Bivand@nhh.no>
2012-12-21 16:39:02 +0000

    version 0.8-21

--- R/lineLabel.R
+++ R/lineLabel.R
@@ -4,20 +4,26 @@
 # Licence GPL v3
 
 lineLabel <- function(line, label,
-                     spar=.6, position = c('above', 'below'),
-                     col = add.text$col,
-                     alpha = add.text$alpha,
-                     cex = add.text$cex,
-                     lineheight = add.text$lineheight,
-                     font = add.text$font,
-                     fontfamily = add.text$fontfamily,
-                     fontface = add.text$fontface,
-                     lty = add.line$lty,
-                     lwd = add.line$lwd, 
-                     col.line = add.line$col,
-                     identifier = 'lineLabel',
+                      spar=.6, position = c('above', 'below'),
+                      textloc = 'constantSlope',
+                      col = add.text$col,
+                      alpha = add.text$alpha,
+                      cex = add.text$cex,
+                      lineheight = add.text$lineheight,
+                      font = add.text$font,
+                      fontfamily = add.text$fontfamily,
+                      fontface = add.text$fontface,
+                      lty = add.line$lty,
+                      lwd = add.line$lwd, 
+                      col.line = add.line$col,
+                      identifier = 'lineLabel',
                      ...){
-  
+  ## line is a list of Line objects
+  ## label and line must be of equal length
+  stopifnot(is.numeric(textloc) | is.character(textloc))
+  stopifnot(length(line) == length(label))
+  if (is.numeric(textloc)) stopifnot(length(line) == length(textloc))
+
   add.text <- trellis.par.get("add.text")
   add.line <- trellis.par.get("add.line")
 
@@ -25,7 +31,8 @@ lineLabel <- function(line, label,
 
   for (i in seq_along(line)) {
     subline <- line[[i]]
-
+    lbl <- label[i]
+    
     coords <- xy.coords(coordinates(subline))
 
     llines(coords,
@@ -33,20 +40,48 @@ lineLabel <- function(line, label,
            identifier = paste(identifier, "line", ## add ID?
              sep = "."))
 
-    if (label !='') { ## Only execute if label is not empty
+    if (lbl !='') { ## Only execute if label is not empty
       ## Where the label is to be placed?
       ## Method "mixed" from lattice::panel.levelplot
       coords <- with(coords, data.frame(x, y))
       slopes <- diff(coords$y) / diff(coords$x)
+      slopes[is.infinite(slopes)] <- NA
+ 
       rx <- range(coords$x)
       ry <- range(coords$y)
-      textloc <- which.min(abs(slopes))
 
-      depth <- pmin(pmin(coords$x - rx[1], rx[2] - coords$x) / diff(rx), 
-                    pmin(coords$y - ry[1], ry[2] - coords$y) / diff(ry))
+      stopifnot(is.character(textloc) | is.numeric(textloc))
+      
+      if (is.character(textloc)){
+        textloc <- match.arg(textloc, c('minSlope', 'maxDepth', 'constantSlope'))
 
-      if (depth[textloc] < 0.05) textloc <- min(which.max(depth), length(slopes)) 
-
+        idxLoc <- switch(textloc,
+                          minSlope = which.min(abs(slopes)),
+                          maxDepth = {
+                            depth <- pmin(pmin(coords$x - rx[1], rx[2] - coords$x) / diff(rx), 
+                                          pmin(coords$y - ry[1], ry[2] - coords$y) / diff(ry))
+                            min(which.max(depth), length(slopes))
+                            },
+                          constantSlope = {
+                            slopeSign <- sign(slopes)
+                            zeros <- which(slopes==0)
+                            for (i in zeros) {
+                              i1 <- max(1, i-1)
+                              i2 <- min(length(slopes), i + 1)
+                              slopeSign[i] <- sign(sum(slopes[c(i1, i2)], na.rm=1))
+                              }
+                            rleSlopes <- rle(slopeSign)
+                            idxCS <- max(which.max(rleSlopes$lengths) - 1, 1)
+                            cumLengths <- cumsum(rleSlopes$lengths)
+                            idxLim <- c(cumLengths[idxCS] +1, cumLengths[idxCS + 1])
+                            idxLim[1]
+                            ## idx1 <- cumsum(rleSlopes$lengths)[idxCS] + 1
+                            ## idx2 <- idx1 + rleSlopes$lengths[idxCS + 1] - 1
+                            ## which(coords[,1] == min(coords[c(idx1, idx2), 1]))
+                            }
+                          )
+        } else {idxLoc <- textloc[i]}
+      
       ## Label: number of characters, width and height
       gp <- gpar(col = col,
                  alpha = alpha,
@@ -54,7 +89,7 @@ lineLabel <- function(line, label,
                  lineheight = lineheight,
                  fontfamily = fontfamily,
                  fontface = fontface)
-      chars <- strsplit(label, '')[[1]]
+      chars <- strsplit(lbl, '')[[1]]
       nchars <- length(chars)
 
 
@@ -74,21 +109,21 @@ lineLabel <- function(line, label,
       labHeight <- max(chHeight)
 
       ##------------------------------------------------------------------##
-      ## Starting at textloc, extract the segment of the line to be
+      ## Starting at idxLoc, extract the segment of the line to be
       ## followed. It must be long enough for the text
       ##------------------------------------------------------------------##
 
       lengths <- LineLength(subline, sum=FALSE, longlat=FALSE)
       nParts <- length(lengths)
-      lengthsLoc <- cumsum(lengths[textloc:nParts])
+      lengthsLoc <- cumsum(lengths[idxLoc:nParts])
 
-      if (labLength > lengthsLoc[nParts - textloc +1]) { ##The segment is not long enough for the label
-        textloc <- 1
+      if (labLength > lengthsLoc[nParts - idxLoc +1]) { ##The segment is not long enough for the label
+        idxLoc <- 1
         segCoords <- apply(coords, 2, mean)
-        slopes <- mean(slopes)
+        slopes <- mean(slopes, na.rm=TRUE)
       } else {
         idxLength <- min(which(lengthsLoc >= labLength))
-        segCoords <- coords[textloc:(textloc+idxLength),]
+        segCoords <- coords[idxLoc:(idxLoc+idxLength),]
         segment <- Line(segCoords)
         lengthSeg <- LineLength(segment)
       }
@@ -120,16 +155,19 @@ lineLabel <- function(line, label,
         D <- labHeight
         ## Unitary perpendicular vector
         perp <- data.frame(x=-diff(segSpline$y), y=diff(segSpline$x))
-        len <- apply(perp, 1, FUN=function(v)sum(sqrt(v^2)))
+        len <- apply(perp, 1, FUN=function(v)sqrt(sum(v^2)))
         perp <- perp/len
 
         ## Points above (+) or below (-) line
 
-        if (position =='below') D <- -abs(D) else D <- abs(D)
-        segOffset <- midPoints +  D*perp
+        if (position =='below') D <- abs(D) else D <- -abs(D)
+        segOffset <- sweep(midPoints, 2, D * colMeans(perp))
+        ##segOffset <- midPoints +  D*perp
 
         segSplineLine <- Line(segOffset) ##Line(segSpline)
         lengthSegSpline <- LineLength(segSplineLine, sum=FALSE)
+
+        ## sp.lines(segSplineLine, col='blue')
 
         ##------------------------------------------------------------------##
         ## A character is placed above a collection of segments whose total
@@ -160,41 +198,49 @@ lineLabel <- function(line, label,
         grid.draw(tg)
 
       } else { ## without 4 distinct x values
-        coordLabs <- coords[textloc,]
-        slopeLabs <- 180/pi * atan(slopes[textloc])
+        coordLabs <- coords[idxLoc,]
+        slopeLabs <- 180/pi * atan(slopes[idxLoc])
         just <- if (position=='below') 'top' else 'bottom'
 
-        tg <- textGrob(label, coordLabs[,1], coordLabs[,2],
+        tg <- textGrob(lbl, coordLabs[,1], coordLabs[,2],
                        just=just,
                        default.units='native',
                        rot=slopeLabs,
                        gp=gp)
         grid.draw(tg)
-      }
-    }
-  }
-    
 
-  ## lapply(1:nchars, FUN=function(i){
-  ##        pg <- circleGrob(coordLabs[i,1], coordLabs[i,2], r=chWidth[i]/2,
-  ##                         default.units='native', gp=gpar(col='red'))
-  ##        grid.draw(pg)
-  ##        })
+
+
+      }
+      ## lapply(1:nchars, FUN=function(i){
+      ##   pg <- circleGrob(coordLabs[i,1], coordLabs[i,2], r=chWidth[i]/2,
+      ##                    default.units='native', gp=gpar(col='red'))
+      ##   grid.draw(pg)
+      ##  })
+    }}
 }
 
-setGeneric('sp.lineLabel', function(object, labels, ...){standardGeneric('sp.lineLabel')})
+setGeneric('sp.lineLabel', function(object, labels, byid=TRUE,...){standardGeneric('sp.lineLabel')})
 
 setMethod('sp.lineLabel',
           signature=(object='Lines'),
-          definition=function(object, labels, ...){
+          definition=function(object, labels, byid=TRUE, ...){
             if (missing(labels)) labels=object@ID
-            lineLabel(object@Lines, label=labels, ...)
+            if (isTRUE(byid)){
+              lengths <- sapply(object@Lines, LineLength)
+              idxMax <- which.max(lengths)
+              labs <- rep('', length(object@Lines))
+              labs[idxMax] <- labels
+            } else {
+              labs <- rep(labels, length(object@Lines))
+            }
+            lineLabel(object@Lines, label=labs, ...)
           }
           )
 
 setMethod('sp.lineLabel',
           signature=(object='SpatialLines'),
-          definition=function(object, labels, ...){
+          definition=function(object, labels, byid=TRUE,...){
             if (missing(labels)){
               ids <- sapply(object@lines, function(l)l@ID)
               labels <- ids
@@ -204,9 +250,22 @@ setMethod('sp.lineLabel',
               line <- object@lines[[i]]
               idLabel <- match(line@ID, names(labels))
               lbl <- if (is.na(idLabel)) '' else as.graphicsAnnot(labels[idLabel])
-              sp.lineLabel(line, lbl, ...)
+              sp.lineLabel(line, lbl, byid=byid,...)
             }
           }
           )
 
 
+setGeneric('label', function(object, text,...){standardGeneric('label')})
+
+setMethod('label', signature=(object='SpatialLines'),
+          definition=function(object, text,...){
+            text <- as.character(text)
+            nfeatures <- length(object)
+            if (!identical(nfeatures, length(text))){
+              stop('Number of labels must equal number of features.')
+            }
+            ids <- sapply(object@lines, function(l)l@ID)
+            names(text) <- ids
+            text
+          })
